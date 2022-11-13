971b39bb617ef34bbe607f701dbc0234a1b51f63
Yusuf Mansur Özer <ymansurozer@gmail.com>
2022-11-14 00:44:55 +0300

    ESLint indent level fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   extends: ["@antfu", "prettier"],
   rules: {
     "antfu/if-newline": "off",
-    "jsonc/indent": ["error", 4],
+    "jsonc/indent": ["error", 2],
     "no-console": "warn",
   },
 };
